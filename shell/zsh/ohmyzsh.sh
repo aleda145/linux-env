1d0e9da4675d982a9258692ae000cb97161f3bd0
Alexander Dahl <alex@dahl.dev>
2021-12-06 20:15:18 +0100

    ohmyzsh takes care of shell change

--- shell/zsh/ohmyzsh.sh
+++ shell/zsh/ohmyzsh.sh
@@ -2,6 +2,5 @@
 
 set -euo pipefail
 
-chsh -s $(which zsh)
 [ -d $HOME/.oh-my-zsh ] && rm -rf $HOME/.oh-my-zsh_old && mv --force $HOME/.oh-my-zsh $HOME/.oh-my-zsh_old
 ZSH= RUNZSH="no" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
