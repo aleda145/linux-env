5da655bb8379772e3e197777401042ce1759a7b7
Alexander Dahl <alex@dahl.dev>
2022-01-21 08:40:49 +0100

    add groups and force yes to docker setup

--- packages/docker.sh
+++ packages/docker.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y \
     lsb-release
 
 # GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # add the key 
 echo \
@@ -39,4 +39,17 @@ curl -SL "https://github.com/docker/compose/releases/download/${latest_release}/
 chmod +x ~/.docker/cli-plugins/docker-compose
 echo -e "\e[31m###########################\e[0m"
 echo -e "\e[31mdocker compose ${latest_release} installed!\e[0m"
+echo -e "\e[31m###########################\e[0m"
+
+# only create the group if it doesnt exist
+if [[ ! $(getent group docker) ]]; then
+  echo "Creating docker group"
+  sudo groupadd docker
+fi
+sudo usermod -aG docker $USER
+
+echo -e "\e[31m###########################\e[0m"
+echo -e "\e[31mAdded ${USER} to docker group!\e[0m"
+echo -e "\e[31mRestart your shell and run:\e[0m"
+echo -e "\e[32m    docker run hello-world\e[0m"
 echo -e "\e[31m###########################\e[0m"
