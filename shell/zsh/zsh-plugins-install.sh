2e1c106251acaa5a9a4f848df5a80ffcbe165a37
Alexander Dahl <alex@dahl.dev>
2022-04-02 20:24:10 +0200

    Fix wrong path and be DRY

--- shell/zsh/zsh-plugins-install.sh
+++ shell/zsh/zsh-plugins-install.sh
@@ -1,6 +1,11 @@
-rm -rf $HOME/.oh-my.zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-rm -rf $HOME/.oh-my.zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-rm -rf $HOME/.oh-my.zsh/custom/plugins/alias-tips
-git clone https://github.com/djui/alias-tips.git $HOME/.oh-my-zsh/custom/plugins/alias-tips
+#!/bin/bash
+set -euxo pipefail
+
+plugins_dir="${HOME}/.oh-my-zsh/custom/plugins"
+
+rm -rf "${plugins_dir}/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${plugins_dir}/zsh-syntax-highlighting"
+rm -rf "${plugins_dir}/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${plugins_dir}/zsh-autosuggestions"
+rm -rf "${plugins_dir}/alias-tips"
+git clone https://github.com/djui/alias-tips.git "${plugins_dir}/alias-tips"
