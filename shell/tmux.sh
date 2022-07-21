22071517b2fcb31e61f145ba0cc08c0b1c5fe94e
Alexander Dahl <alex@dahl.dev>
2022-07-21 15:05:16 +0200

    Add tmux plugins

--- shell/tmux.sh
+++ shell/tmux.sh
@@ -6,4 +6,6 @@ rm -rf "${HOME}/.tmux"
 git clone https://github.com/gpakosz/.tmux.git "${HOME}/.tmux"
 ln -s -f "${HOME}/.tmux/.tmux.conf" "${HOME}/.tmux.conf"
 cp "${HOME}/.tmux/.tmux.conf.local" "${HOME}/.tmux.conf.local"
-echo "setw -g mouse on" >> "${HOME}/.tmux.conf.local"
+echo "setw -g mouse on
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+set -g @plugin 'laktak/extrakto'" >> "${HOME}/.tmux.conf.local"
