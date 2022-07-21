ebb0aa847fa23e588cb40716a9936098aa3ff643
Alexander Dahl <alex@dahl.dev>
2022-07-21 14:39:24 +0200

    Fix duplicate caused by appendimg sdk source to apt

--- packages/gcp.sh
+++ packages/gcp.sh
@@ -4,7 +4,7 @@ echo -e "\e[31mInstalling gcloud\e[0m"
 echo -e "\e[31m###########################\e[0m"
 
 sudo apt-get install apt-transport-https ca-certificates gnupg
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 echo "now run cloud init"
