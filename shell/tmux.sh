77811dd0184de9e13f92d501220362aba1162f88
Alexander Dahl <alex@dahl.dev>
2022-07-21 22:07:56 +0200

    Add continuum

--- shell/tmux.sh
+++ shell/tmux.sh
@@ -8,4 +8,6 @@ ln -s -f "${HOME}/.tmux/.tmux.conf" "${HOME}/.tmux.conf"
 cp "${HOME}/.tmux/.tmux.conf.local" "${HOME}/.tmux.conf.local"
 echo "setw -g mouse on
 set -g @plugin 'tmux-plugins/tmux-resurrect'
-set -g @plugin 'laktak/extrakto'" >> "${HOME}/.tmux.conf.local"
+set -g @plugin 'laktak/extrakto'
+set -g @plugin 'tmux-plugins/tmux-continuum'
+" >> "${HOME}/.tmux.conf.local"
