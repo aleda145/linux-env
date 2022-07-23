9e498dd0b60e73ed18a873ae48eef241134a58a4
Alexander Dahl <alex@dahl.dev>
2022-07-23 11:28:22 +0200

    Change dir to oh my tmux to resolve conflicts

--- shell/tmux.sh
+++ shell/tmux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -euo pipefail
-rm -rf "${HOME}/.tmux"
+rm -rf "${HOME}/.oh-my-tmux"
 
-git clone https://github.com/gpakosz/.tmux.git "${HOME}/.tmux"
-ln -s -f "${HOME}/.tmux/.tmux.conf" "${HOME}/.tmux.conf"
-cp "${HOME}/.tmux/.tmux.conf.local" "${HOME}/.tmux.conf.local"
+git clone https://github.com/gpakosz/.tmux.git "${HOME}/.oh-my-tmux"
+ln -s -f "${HOME}/.oh-my-tmux/.tmux.conf" "${HOME}/.tmux.conf"
+cp "${HOME}/.oh-my-tmux/.tmux.conf.local" "${HOME}/.tmux.conf.local"
 echo "setw -g mouse on
 set -g @plugin 'tmux-plugins/tmux-resurrect'
 set -g @plugin 'laktak/extrakto'
