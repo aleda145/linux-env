41f4252bc36bae905523f806a8cbf038497be89d
Alexander Dahl <alex@dahl.dev>
2023-03-11 11:10:29 +0100

    Bump logseq version

--- packages/logseq.sh
+++ packages/logseq.sh
@@ -5,6 +5,6 @@ echo -e "\e[31m###########################\e[0m"
 echo -e "\e[31mInstalling logseq\e[0m"
 echo -e "\e[31m###########################\e[0m"
 
-wget https://github.com/logseq/logseq/releases/download/0.8.11/Logseq-linux-x64-0.8.11.AppImage -O logseq
+wget https://github.com/logseq/logseq/releases/download/0.8.18/Logseq-linux-x64-0.8.18.AppImage -O logseq
 chmod +x logseq
 mv logseq "${HOME}/bin"
