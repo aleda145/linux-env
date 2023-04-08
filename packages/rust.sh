debfb973352cf30dd565344dffbe5cff7a1fc0ce
Alexander Dahl <alex@dahl.dev>
2023-04-08 23:05:20 +0200

    Add rust

---
+++ packages/rust.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "\e[31m###########################\e[0m"
+echo -e "\e[31mInstalling Rust 🦀\e[0m"
+echo -e "\e[31m###########################\e[0m"
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+source "$HOME/.cargo/env" # Source so we can go ahead and install cargoes directly
+
+cargo install bottom
+cargo install du-dust
+cargo install gping
