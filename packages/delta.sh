0220e1e1842622d09206c3429fe171aef6b6c8ff
Alexander Dahl <alex@dahl.dev>
2026-01-24 18:16:51 +0100

    Bump delta

--- packages/delta.sh
+++ packages/delta.sh
@@ -3,6 +3,6 @@ echo -e "\e[31m###########################\e[0m"
 echo -e "\e[31mInstalling Delta\e[0m"
 echo -e "\e[31m###########################\e[0m"
 
-wget "https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_amd64.deb" -O delta.deb
+wget "https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb" -O delta.deb
 sudo dpkg -i delta.deb
 rm delta.deb
